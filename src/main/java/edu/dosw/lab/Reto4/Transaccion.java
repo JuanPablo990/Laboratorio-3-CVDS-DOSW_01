0b24434a4a4fa92fd20d36bdedbbade826feb412
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-09-03 23:57:20 -0500

    se han añadido las clases transaccion y validar cuenta

---
+++ src/main/java/edu/dosw/lab/Reto4/Transaccion.java
@@ -0,0 +1,33 @@
+package edu.dosw.lab.Reto4;
+
+import java.util.Date;
+
+public class Transaccion {
+    private String idTransaccion;
+    private TipoTransaccion tipo;
+    private double monto;
+    private Date fecha;
+
+    public Transaccion(String idTransaccion, TipoTransaccion tipo, double monto, Date fecha) {
+        this.idTransaccion = idTransaccion;
+        this.tipo = tipo;
+        this.monto = monto;
+        this.fecha = fecha;
+    }
+
+    public String getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public TipoTransaccion getTipo() {
+        return tipo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+}
---
+++ src/main/java/edu/dosw/lab/Reto4/ValidarCuenta.java
@@ -0,0 +1,19 @@
+package edu.dosw.lab.Reto4;
+
+public class ValidarCuenta {
+    public ValidarCuenta() {}
+
+    public boolean validarLongitud(String numeroCuenta) {
+        return numeroCuenta != null && numeroCuenta.length() == 10;
+    }
+
+    public boolean validarPrefijo(String numeroCuenta, Banco banco) {
+        if (numeroCuenta == null || banco == null) return false;
+        String prefijo = numeroCuenta.substring(0, 3);
+        return prefijo.equals(banco.getCodigoBanco());
+    }
+
+    public boolean validarFormato(String numeroCuenta) {
+        return numeroCuenta != null && numeroCuenta.matches("\\d+");
+    }
+}
